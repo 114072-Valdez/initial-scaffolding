8dd76aaacb15461bdc0b2da1f7b5a9bfb11aec5c
114072_VALDEZ_GERARDO <114072@tecnicatura.frc.utn.edu.ar>
2024-06-09 23:16:52 -0300

    omisiones

--- src/main/java/ar/edu/utn/frc/tup/lc/iii/dtos/DummyDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iii/dtos/DummyDto.java
@@ -1,4 +1,16 @@
 package ar.edu.utn.frc.tup.lc.iii.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DummyDto {
+    private Long id;
+
+    @JsonProperty("description")
+    private String description;
 }
--- src/main/java/ar/edu/utn/frc/tup/lc/iii/entities/DummyEntity.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iii/entities/DummyEntity.java
@@ -1,7 +1,13 @@
 package ar.edu.utn.frc.tup.lc.iii.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class DummyEntity {
 
